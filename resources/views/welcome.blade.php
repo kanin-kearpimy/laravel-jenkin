1d5c1cf27ddecdba142699c953520c6e7a533ac7
kaninkearpimy23814 <kanin23814@gmail.com>
2021-04-25 19:28:42 +0700

    revert to original

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -21,10 +21,10 @@
         </style>
     </head>
     <body class="antialiased">
-        <h1>Change Code for Jenkins</h1>
+        {{-- <h1>Change Code for Jenkins</h1>
         <h2>==========</h2>
-        <h3>-----</h3>
-        {{-- <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
+        <h3>-----</h3> --}}
+        <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
             @if (Route::has('login'))
                 <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
                     @auth
@@ -130,6 +130,6 @@
                     </div>
                 </div>
             </div>
-        </div> --}}
+        </div>
     </body>
 </html>
